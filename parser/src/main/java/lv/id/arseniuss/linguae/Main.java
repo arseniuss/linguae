27fb5fa33442609e357c18a9a7724780b6afa744
Armands Arseniuss Skolmeisters <arseniuss@arseniuss.id.lv>
2025-03-23 00:54:32 +0200

    Made parser an CLI program

--- parser/src/main/java/lv/id/arseniuss/linguae/Main.java
+++ parser/src/main/java/lv/id/arseniuss/linguae/Main.java
@@ -2,6 +2,6 @@ package lv.id.arseniuss.linguae;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Sīc locūtus language repository parser");
     }
 }
